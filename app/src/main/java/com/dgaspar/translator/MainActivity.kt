dccad755069ffd35112a1a84ba14cf15ac229fc4
Daniel Gaspar Gonçalves <d-gaspar@users.noreply.github.com>
2021-02-15 21:28:17 -0300

    adjust design, try to install translate model

--- app/src/main/java/com/dgaspar/translator/MainActivity.kt
+++ app/src/main/java/com/dgaspar/translator/MainActivity.kt
@@ -3,9 +3,13 @@ package com.dgaspar.translator
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.LinearLayout
-import android.widget.Spinner
+import android.view.KeyEvent
+import android.view.View
+import android.widget.*
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,38 @@ class MainActivity : AppCompatActivity() {
         // set background color
         var mainLayout : LinearLayout = findViewById(R.id.mainLayout)
         mainLayout.setBackgroundColor(Color.parseColor("#deebf7"))
+
+        // editTexts
+        var inputEditText : EditText = findViewById(R.id.inputText)
+        var outputEditText : EditText = findViewById(R.id.outputText)
+
+        /////////////////////////////////////////////////////////////////////////////////////
+
+        // configure translator
+
+        val options = TranslatorOptions.Builder()
+                .setSourceLanguage(TranslateLanguage.ENGLISH)
+                .setTargetLanguage(TranslateLanguage.PORTUGUESE)
+                .build()
+        val englishPortugueseTranslator = Translation.getClient(options)
+
+        var conditions = DownloadConditions.Builder()
+                .requireWifi()
+                .build()
+        englishPortugueseTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener {
+                    // Model downloaded successfully. Okay to start translating.
+                    // (Set a flag, unhide the translation UI, etc.)
+                    inputEditText.isEnabled = true
+                    inputEditText.isFocusable = true
+                }
+                .addOnFailureListener { exception ->
+                    // Model couldn’t be downloaded or other internal error.
+                    // ...
+                    Toast.makeText(this, "Não foi possível baixar o pacote de tradução!", Toast.LENGTH_SHORT).show()
+                }
+
+        /////////////////////////////////////////////////////////////////////////////////////
 
         // input language
         setContentOnDropDownView(
@@ -28,6 +64,30 @@ class MainActivity : AppCompatActivity() {
             R.id.outputLanguage,
             arrayOf("Português", "Inglês")
         )
+
+        /////////////////////////////////////////////////////////////////////////////////////
+
+        // watch editText
+        var outputText : String = ""
+
+        inputEditText.setOnKeyListener(View.OnKeyListener{ v, keyCode, event ->
+            // if(event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER){
+            if(event.action == KeyEvent.ACTION_UP){
+
+                // get input text
+                outputText = inputEditText.text.toString()
+
+                // translate
+
+
+                // set outputText
+                println(inputEditText.text)
+                outputEditText.setText(outputText)
+
+                true
+            }
+            false
+        })
     }
 
     fun setContentOnDropDownView(
